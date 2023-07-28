bb7df058f1312d03292956431b31e08693194b48
Matt Filetto <matt.filetto@gmail.com>
2023-07-28 07:15:49 +0000

    gs101: add adaptive charging

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -300,6 +300,10 @@ BOARD_USES_CURL := true
 # Sensor HAL
 BOARD_USES_EXYNOS_SENSORS_DUMMY := true
 
+# Adaptive charging
+PRODUCT_COPY_FILES += \
+    device/google/gs101/conf/adaptivecharging.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/adaptivecharging.xml
+
 # VISION
 # Exynos vision framework (EVF)
 #TARGET_USES_EVF := true
